e209f5fa87400333401f93702f4898e363ccb77e
mPooja-15 <mishrapooja8128@gmail.com>
2025-03-12 13:28:08 +0530

    fix: converted agreement page in bootstrap css

--- src/Pages/Agreementes/Agreementes.jsx
+++ src/Pages/Agreementes/Agreementes.jsx
@@ -16,10 +16,10 @@ const Agreements = () => {
 
     return (
         <div>
-            <div className="w-full absolute top-0 overflow-hidden z-0">
-                <figure className="mb-0 top_bg_fig">
-                    <img src={bodyBg} width="100%" alt="Background" />
-                    <span className="absolute right-0 top-2">
+            <div className="position-absolute top-0 w-100 overflow-hidden z-0">
+                <figure className="mb-0">
+                    <img src={bodyBg} className="w-100" alt="Background" />
+                    <span className="position-absolute end-0 top-2">
                         <img src={boryGroupLeft} alt="left bg icon" />
                     </span>
                 </figure>
@@ -28,132 +28,103 @@ const Agreements = () => {
                 </div>
             </div>
 
-            <div className="min-h-[550px] bg-white max-w-full w-[1194px] my-0 mx-auto pb-6 relative z-50 rounded-2xl shadow-lg">
-                <div className="px-6">
-                    <h1 className="text-2xl font-semibold text-[#00A481] py-4 mb-7 border-b border-solid border-[#EAEAEA] text-center">
-                        {t("all_agreements")}
-                    </h1>
-                </div>
-                <div className="px-6 py-0">
-                    <div className="h-[646px] overflow-y-scroll custom-scrollbar">
-                        <div className="mr-7">
-                            <Nav variant="tabs" className="d-flex px-3 pt-2 px-md-4 border-bottom">
-                                <Nav.Item>
-                                    <Nav.Link
-                                        className={`px-3 focus:!border-transparent hover:!border-transparent ${activeTab === "all" ? "active-tab" : ""}`}
-                                        onClick={() => setActiveTab("all")}
-                                    >
-                                        {t("all_agreements")}
-                                    </Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <Nav.Link
-                                        className={`px-3 focus:!border-transparent hover:!border-transparent ${activeTab === "recent" ? "active-tab" : ""}`}
-                                        onClick={() => setActiveTab("recent")}
-                                    >
-                                        {t("recent_agreements")}
-                                    </Nav.Link>
-                                </Nav.Item>
-                            </Nav>
+            <div className="container bg-white my-5 p-4 shadow-lg rounded-3 position-relative z-1">
+                <h1 className="text-center text-success py-3 mb-4 border-bottom border-[#EAEAEA]">
+                    {t("all_agreements")}
+                </h1>
+                <div className="overflow-auto custom-scrollbar" style={{ maxHeight: "646px" }}>
+                    <div className="me-4">
+                        <Nav variant="tabs" className="mb-3 border-bottom border-[#EAEAEA]">
+                            <Nav.Item>
+                                <Nav.Link className={`px-3 border-0 focus-ring-transparent hover-border-transparent ${activeTab === "all" ? "active-tab" : ""}`} onClick={() => setActiveTab("all")}>
+                                    {t("all_agreements")}
+                                </Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link className={`px-3 border-0 focus-ring-transparent hover-border-transparent ${activeTab === "recent" ? "active-tab" : ""}`} onClick={() => setActiveTab("recent")}>
+                                    {t("recent_agreements")}
+                                </Nav.Link>
+                            </Nav.Item>
+                        </Nav>
 
-                            <div className="tab-content mt-4">
-                                {activeTab === "all" && (
+                        <div className="tab-content mt-3">
+                            {activeTab === "all" && (
+                                <div>
+                                    <div className="mb-4 position-relative w-75 border border-[#D6D6D6] rounded py-2 px-3">
+                                        <div className="d-flex">
+                                            <input
+                                                type="text"
+                                                className="form-control border-0 p-0"
+                                                placeholder={t("search_placeholder")}
+                                            />
+                                            <button className="btn p-0" type="button">
+                                                <img src={search} alt="Search" />
+                                            </button>
+                                        </div>
+                                    </div>
+
+                                    <div className="mb-4 d-flex align-items-end gap-3">
+                                        <div className="w-100">
+                                            <label className="form-label">{t("from_date")}</label>
+                                            <input type="date" className="form-control" value={fromDate} onChange={(e) => setFromDate(e.target.value)} />
+                                        </div>
+                                        <div className="w-100">
+                                            <label className="form-label">{t("until_date")}</label>
+                                            <input type="date" className="form-control" value={toDate} onChange={(e) => setToDate(e.target.value)} />
+                                        </div>
+                                        <div className="w-100">
+                                            <label className="form-label">{t("agreement_status")}</label>
+                                            <select className="form-select">
+                                                <option>{t("status_agreement")}</option>
+                                                <option value="1">Option 1</option>
+                                                <option value="2">Option 2</option>
+                                                <option value="3">Option 3</option>
+                                            </select>
+                                        </div>
+                                        <div className="w-100 d-flex align-items-center">
+                                            <label className="me-2">{t("invoice_label")}</label>
+                                            <div className="form-check form-switch">
+                                                <input className="form-check-input" type="checkbox" id="invoiceToggle" />
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <button className="btn btn-outline-success rounded-pill px-5 py-2">{t("show_button")}</button>
+                                        </div>
+                                    </div>
+
+                                    <div className="d-flex mb-5">
+                                        <span className="d-flex align-items-center gap-3 py-2 px-3 rounded-pill" style={{ background: "#E9FAF4" }}>
+                                            <span>{t("generated")}</span>
+                                            <img src={remove_icon} alt="" className="img-fluid" style={{ width: "12px" }} />
+                                        </span>
+                                    </div>
                                     <div>
-                                        <div className="w-full d-flex">
-                                            <div className="position-relative d-flex justify-between">
-                                                <span className="position-absolute mt-3 px-3 end-0">
-                                                    <img src={search} alt="" className="w-4 h-4" />
-                                                </span>
-                                                <input
-                                                    type="text"
-                                                    className="form-control end-0"
-                                                    placeholder={t("search_placeholder")}
-                                                    style={{ width: "726px", height: "44px", borderRadius: "5px", border: "1px solid #D6D6D6" }}
-                                                />
-                                            </div>
-                                        </div>
-
-                                        <div className="row justify-center align-items-center mt-4">
-                                            <div className="col fw-semibold">
-                                                <label className="d-flex w-100">{t("from_date")}</label>
-                                                <div className="form-control d-flex align-items-center">
-                                                    <input
-                                                        type="date"
-                                                        className="border-0 w-100"
-                                                        value={fromDate}
-                                                        onChange={(e) => setFromDate(e.target.value)}
-                                                    />
-                                                </div>
-                                            </div>
-                                            <div className="col fw-semibold">
-                                                <label className="d-flex w-100">{t("until_date")}</label>
-                                                <div className="form-control d-flex align-items-center">
-                                                    <input
-                                                        type="date"
-                                                        className="border-0 w-100"
-                                                        value={toDate}
-                                                        onChange={(e) => setToDate(e.target.value)}
-                                                    />
-                                                </div>
-                                            </div>
-                                            <div className="col fw-semibold">
-                                                <label className="d-flex w-100">{t("agreement_status")}</label>
-                                                <select className="form-select focus:!shadow-none">
-                                                    <option>{t("status_agreement")}</option>
-                                                    <option value="1">Option 1</option>
-                                                    <option value="2">Option 2</option>
-                                                    <option value="3">Option 3</option>
-                                                </select>
-                                            </div>
-                                            <div className="col-auto d-flex align-items-center mt-3">
-                                                <label className="form-check-label me-2">{t("invoice_label")}</label>
-                                                <div className="form-check form-switch">
-                                                    <input className="form-check-input" type="checkbox" id="invoiceToggle" />
-                                                </div>
-                                            </div>
-                                            <div className="col-auto mt-3">
-                                                <button className="btn btn-outline-success !rounded-full lh-1">{t("show_button")}</button>
-                                            </div>
-                                        </div>
-
-                                        <div className="d-flex mt-4">
-                                            <span
-                                                className="bg-emerald-500 flex gap-4 py-2.5 px-3 rounded-[18px] h-10 align-items-center"
-                                                style={{ backgroundColor: "#E9FAF4" }}
-                                            >
-                                                <span>{t("generated")}</span>
-                                                <img src={remove_icon} alt="" className="w-3" />
-                                            </span>
-                                        </div>
-                                        <div className="mt-11">
-                                            <AgreementsTable />
-                                        </div>
+                                        <AgreementsTable />
                                     </div>
-                                )}
+                                </div>
+                            )}
 
-                                {activeTab === "recent" && (
-                                    <div className="text-center py-4">
-                                        <p>{t("no_recent_agreements")}</p>
-                                    </div>
-                                )}
-                            </div>
+                            {activeTab === "recent" && (
+                                <div className="text-center py-4">
+                                    <p>{t("no_recent_agreements")}</p>
+                                </div>
+                            )}
                         </div>
                     </div>
                 </div>
             </div>
-
             <style>
                 {`
-      .nav-tabs .nav-link {
-        color: #848484;
-        border-bottom: 2px solid transparent;
-        cursor: pointer;
-      }
-      .nav-tabs .nav-link.active-tab {
-        color: #00A6A4 !important;
-        border-bottom: 2px solid #00A6A4 !important;
-      }
-    `}
+                    .nav-tabs .nav-link {
+                        color: #848484;
+                        border-bottom: 2px solid transparent;
+                        cursor: pointer;
+                    }
+                    .nav-tabs .nav-link.active-tab {
+                        color: #00A6A4 !important;
+                        border-bottom: 2px solid #00A6A4 !important;
+                    }
+                `}
             </style>
         </div>
     );
